1361aacdcb9506c5e8426cb35bd99370d7836001
musergi <sergi.mus@gmail.com>
2021-03-06 08:57:17 +0100

    Added more combinations to the default run script

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 PYTHON=venv/bin/python
+
+# Generate small
 mkdir -p dataset
-$PYTHON generate.py --layer_size_start 8 --layer_size_stop 32 \
-    --layer_size_step 8 --layer_count_start 1 --layer_count_stop 3 \
+$PYTHON generate.py --layer_size_start 32 --layer_size_stop 257 \
+    --layer_size_step 32 --layer_count_start 1 --layer_count_stop 3 \
     --layer_count_step 1 --out_dir dataset
